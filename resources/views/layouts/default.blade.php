29bb614b515b75b2eb2096d4f2b9db7d78f5f21a
bipinfutureprofilez <bipinkr7742@gmail.com>
2024-12-26 15:59:05 +0530

    working on layout design

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,4 +1,14 @@
-@include('layouts.header')
-@include('layouts.sidebar')
-@yield('content')
-@include('layouts.footer')
+<div class="flex flex-wrap gap-x-[60px] ">
+    <div class="w-[190px]">
+        @include('layouts.sidebar')
+    </div>
+    <div class="w-[calc(100%-250px)]">
+        <div class="admin-header">
+            @include('layouts.header')
+        </div>
+        @yield('content')
+    </div>
+    <div class="w-full bg-[#13103A] text-center">
+        @include('layouts.footer')
+    </div>
+</div>

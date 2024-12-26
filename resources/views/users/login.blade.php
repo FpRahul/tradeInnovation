9c4cab53a28087c44ba7ee4c49d11d94883e8cba
RC-Sputnik <rahul@efulfilment.eu>
2024-12-26 13:36:37 +0530

    Iitilising Tailwind

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    This is login page
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      @vite('resources/css/app.css')
+    </head>
+    <body>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </body>
+    </html>
 @endsection

2dca20160ac9e05237bed62f8f3d478af14c8b0c
RC-Sputnik <rahul@efulfilment.eu>
2024-12-26 13:45:14 +0530

    finalising everything

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,15 +1,4 @@
 @extends('layouts.login')
 @section('content')
-<html>
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      @vite('resources/css/app.css')
-    </head>
-    <body>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </body> 
-    </html>
+Login page form and content
 @endsection

36d882d7a60781304a8bd37cea8dd286d3e5fa41
bipinfutureprofilez <bipinkr7742@gmail.com>
2025-01-02 17:05:44 +0530

    js issue resolved

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -25,7 +25,7 @@
             <div class="admin-header pl-[30px]">
                 @include('layouts.header')
             </div>
-            <div class="main-right-content pr-[22px] pt-[23px] h-[calc(100vh-129px)] max-h-[calc(100vh-129px)] overflow-y-auto pl-[30px]">
+            <div class="main-right-content pr-[22px] py-[23px] h-[calc(100vh-129px)] max-h-[calc(100vh-129px)] overflow-y-auto pl-[30px]">
                 @yield('content')
             </div>
         </div>
@@ -37,19 +37,59 @@
         jQuery(document).on('click', '.dropdown .button', function() {
             jQuery(this).parent().find('.dropdown_menu').toggleClass('hidden');
         });
-    
+
+        jQuery(document).on('click', 'table .dropdown .button', function() {
+            const $menu = jQuery(this).parent().find('.dropdown_menus');
+            let dataId = jQuery(this).attr('data-id');
+            if (!dataId) {
+                let random = Math.floor(Math.random() * 10);
+                jQuery(this).attr('data-id', random);
+                jQuery($menu).attr('data-id', random);
+            }
+            if ($menu.hasClass('hidden')) {
+
+                const offset = jQuery(this).offset();
+                $menu.appendTo('body').css({
+                    position: 'absolute',
+                    top: offset.top + jQuery(this).outerHeight(),
+                    left: offset.left - 85,
+                    zIndex: 9999,
+                }).removeClass('hidden');
+            } else {
+                let ele = jQuery(`.dropdown_menus[data-id="${dataId}"]`);
+                console.log('55555: ', ele);
+                jQuery(ele).addClass('hidden');
+                jQuery(this).parent().append(ele);
+            }
+        });
+
+        jQuery(document).click(function(e) {
+            e.stopPropagation();
+            var container = jQuery("table .dropdown");
+
+            //check if the clicked area is dropDown or not
+            if (container.has(e.target).length === 0) {
+                jQuery('.dropdown_menus').each(function() {
+                    const dataId = jQuery(this).attr('data-id');
+                    let ele = jQuery(`table .dropdown .button[data-id="${dataId}"]`);
+                    jQuery(this).addClass('hidden');
+                    jQuery(ele).parent().append(jQuery(this));
+                });
+            }
+        });
+
         jQuery(document).click(function(e) {
             e.stopPropagation();
             var container = jQuery(".dropdown");
-    
+
             //check if the clicked area is dropDown or not
             if (container.has(e.target).length === 0) {
                 jQuery('.dropdown_menu').addClass('hidden');
             }
         });
-    
+
         jQuery(document).on('click', '.accordian .item>a', function() {
-            
+
             if (!jQuery(this).hasClass('nav-link')) {
                 jQuery(this).parent().find('.accordian_body').slideDown();
                 jQuery(this).parent().siblings().find('.accordian_body').slideUp();
@@ -61,13 +101,7 @@
                 jQuery(this).parent().find('.accordian_body').slideToggle();
                 jQuery(this).parent().siblings().find('.accordian_body').slideUp();
             }
-    
-        })
-    
-        // jQuery('').append()
-    
-        jQuery(document).on('click', '.has-submenu > a', function(){
-            
+
         })
     </script>
 </body>

f2dec171ea69665539a80eff1fa145367878e105
bipinfutureprofilez <bipinkr7742@gmail.com>
2025-01-08 15:29:17 +0530

    responsive working2

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
 
     <title>Trade Innovation</title>
 

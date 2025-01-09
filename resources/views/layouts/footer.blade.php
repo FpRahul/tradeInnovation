5ae54f88962eb4c5c81ea4ad00105858036fac9b
Piyush Vijay <piyush.vijay@futureprofilez.com>
2025-01-09 17:39:34 +0530

    update the image field with the validation on user module ,preview image

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,1 @@
-<p class="text-center font-[Inter] text-[13px] font-[400] text-[#FFFFFF] leading-[19px] tracking-[-0.02em]">Copyright®2024 Company All right Reserved</p>
+<p class="text-center font-[Inter] text-[13px] font-[400] text-[#FFFFFF] leading-[19px] tracking-[-0.02em]">Copyright®{{date('Y')}} Company All right Reserved</p>

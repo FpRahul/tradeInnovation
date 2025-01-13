2715970157f60b7b3706de93c5016d42936f052a
Sputnik <rahul@fpdemo.com>
2025-01-13 18:06:37 +0530

    role module with add and edit functionlity

---
+++ resources/views/components/menuassigned.blade.php
@@ -0,0 +1,6 @@
+
+<div>
+    <span class="text-[14px] font-[400] leading-[16px] text-[#ffffff] capitalize bg-[#13103A] rounded-[10px] px-[15px] py-[7px] ">dashboard</span>
+    <span class="text-[14px] font-[400] leading-[16px] text-[#ffffff] capitalize bg-[#13103A] rounded-[10px] px-[15px] py-[7px] "> {{ $userRole }}</span>
+    <span class="text-[14px] font-[400] leading-[16px] text-[#ffffff] capitalize bg-[#13103A] rounded-[10px] px-[15px] py-[7px] ">Tasks</span>
+</div>

78dae9aadb40c2c6fb81cbe753ee4fe2eee09693
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2025-01-31 18:33:53 +0530

    worked on task document verification

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\LeadTask;
+use App\Models\LeadTaskDetail;
+use App\Models\LeadLog;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 
 use App;
 class TasksController extends Controller
@@ -13,6 +18,7 @@ class TasksController extends Controller
     public function index(Request $request){
         $header_title_name="Tasks";
             $taskDetails = LeadTask::with([ 'user','lead' ,'leadTaskDetails' ,'leadServices.service','leadServices.subservice']);
+            
             $searchKey = $request->input('key') ?? '';
             $requestType = $request->input('requestType') ?? '';
             if ($searchKey) {
@@ -53,27 +59,74 @@ class TasksController extends Controller
         $header_title_name="Lead Logs";
         return view($this->viewPath.'logs',compact('header_title_name'));
     }
-
     public function detail($id){
-        
         $header_title_name="Lead action & details";
-        $taskDetails = LeadTask::with(['user', 'lead', 'leadAssign', 'leadTaskDetails', 'leadServices.service', 'leadServices.subservice'])
+        $taskDetails = LeadTask::with(['user', 'lead', 'leadTaskDetails', 'leadServices.service', 'leadServices.subservice'])
         ->where('id', $id)
         ->get();
         return view('tasks/detail',compact('header_title_name', 'taskDetails'));
     }
-
     public function chekDuplication($id){
         $header_title_name="Lead action & details";
-        $taskDetails = LeadTask::with(['user', 'lead', 'leadAssign', 'leadTaskDetails', 'leadServices.service', 'leadServices.subservice'])
+        $taskDetails = LeadTask::with(['user', 'lead', 'leadTaskDetails', 'leadServices.service', 'leadServices.subservice'])
         ->where('id', $id)
         ->get();
-        $users = User::where('role', '>', '4')->where('archive', 1)->where('status', 1);
-        return view('tasks/detail',compact('header_title_name', 'taskDetails', 'users'));
+        $taskID = $id;
+    
+        $users = User::where('role', '>', '4')->where('archive', 1)->where('status', 1)->get();
+        return view('tasks/tradeMark/check_duplication',compact('header_title_name', 'taskID','taskDetails', 'users'));
+    }
+
+    public function documentVerified(Request $request , $id){
+        
+        //   dd($request->all());
+           $rule = [
+            'status' => 'required',
+            'verified' => 'required',
+            'assignUser' => 'required',
+            'deadline' => 'nullable',
+            'description' => 'nullable'
+           ];
+
+           $validtor = Validator::make($request->all(), $rule);
+           if($validtor->fails()){
+            return redirect()->back()->withErrors($validtor)->withInput();
+           }
+           if($id){
+              $existedTaskDetails = LeadTask::find($id);
+              $existedLeadTaskDetails = LeadTaskDetail::where('task_id', $id)->first();
+              $existedTaskDetails->user_id = $request->assignUser;
+              $existedTaskDetails->assign_by = Auth::id();
+              $existedTaskDetails->task_description = $request->description;
+              if($existedTaskDetails->save()){
+                $existedLeadTaskDetails->dead_line = $request->deadline;
+                $existedLeadTaskDetails->status = $request->status;
+                $existedLeadTaskDetails->status_date = $request->verified;
+                $existedLeadTaskDetails->save();
+
+                    $LeadLog = new LeadLog();
+                    $LeadLog->user_id = $request->assignUser;
+                    $LeadLog->lead_id =  $existedTaskDetails->lead_id;
+                    $LeadLog->task_is = $existedTaskDetails->id;
+                    $LeadLog->assign_by = Auth::id();
+                    $LeadLog->description = $request->description;
+                    $LeadLog->save();
+                    return redirect()->back()->with('success', 'Document verified successfully');
+                }
+
+              }
+
+              
+
+
+           }
+
+
+
     }
    
 
     
-}
+
 
 

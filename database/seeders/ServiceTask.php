f1c824fa60ff4bf0e44977c91f2b917b3c4f4431
Sputnik <rahul@fpdemo.com>
2025-02-12 18:33:55 +0530

    update the seeder for the service stages

--- database/seeders/ServiceTask.php
+++ database/seeders/ServiceTask.php
@@ -33,13 +33,13 @@ class ServiceTask extends Seeder
             [
                 'service_id' => '1',
                 'title' => 'Document verification',
-                'description' => 'Verify documents internally which will be used to file application.',
+                'description' => 'Verify documents internally which will be used to file the application.',
                 'stage' => '0',
-            ],
+            ], 
             [
                 'service_id' => '1',
-                'title' => 'Client approval for documentation',
-                'description' => 'Take approval from the client.',
+                'title' => 'Client approval on documentation',
+                'description' => 'Client confirmation on the documents which is used to file the application',
                 'stage' => '0',
             ],
             [
@@ -50,15 +50,75 @@ class ServiceTask extends Seeder
             ],
             [
                 'service_id' => '1',
+                'title' => 'Formality Check',
+                'description' => 'Update the formality check status.',
+                'stage' => '0',
+            ],
+            [
+                'service_id' => '1',
                 'title' => 'Initial Examination',
                 'description' => 'Update the initial examination status.',
                 'stage' => '0',
             ],
             [
                 'service_id' => '1',
-                'title' => 'Formality Check',
-                'description' => 'Update the formality check status.',
+                'title' => 'Objection',
+                'description' => 'Add reply on the objection within 30 days.',
+                'stage' => '8',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Awaited for hearing (Objection)',
+                'description' => 'Add the hearing date provided by the court for the raised objection',
+                'stage' => '8',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Show cause hearing (Objection)',
+                'description' => 'Update the show cause hearing status / or next hearing date',
+                'stage' => '8',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Publish application',
+                'description' => 'Add the details of the published application in the system.',
                 'stage' => '0',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Opposition',
+                'description' => 'Update the opposition status.',
+                'stage' => '0',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Inform client and payment confirmation',
+                'description' => 'Inform client about the opposition and mark the payment status.',
+                'stage' => '13',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Counter Statment',
+                'description' => 'Add the counter statment on the received oppostion.',
+                'stage' => '13',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Submit Evidence',
+                'description' => 'Submitt the evidence on the received opposition.',
+                'stage' => '13',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Awaited for hearing (Opposition)',
+                'description' => 'Add the hearing date provided by the court for the raised opposition',
+                'stage' => '13',
+            ],
+            [
+                'service_id' => '1',
+                'title' => 'Show cause hearing (Opposition)',
+                'description' => 'Update the show cause hearing status / or next hearing date',
+                'stage' => '13',
             ],
             //patnet
             [

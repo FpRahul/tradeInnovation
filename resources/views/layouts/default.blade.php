d3510d03bec4fe66da2c1ed8fc1e9aac6b80a0bb
fpmukesh <mukesh@fpdemo.com>
2025-04-09 11:58:41 +0530

    footer file update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -46,7 +46,7 @@
                 @yield('content')
             </div>
         </div>
-        <div class="w-full mt-[auto] bg-[#13103A] text-center h-[54px] flex items-center justify-center ">
+        <div class="fixed bottom-0 z-[9] w-full mt-[auto] bg-[#13103A] text-center h-[54px] flex items-center justify-center ">
             @include('layouts.footer')
         </div>
     </div>

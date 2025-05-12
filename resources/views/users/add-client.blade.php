f96bcf9b6a8e8e195ae862d303713a1f7e4aa7c5
Sputnik <rahul@fpdemo.com>
2025-05-12 10:59:03 +0530

    changes

--- resources/views/users/add-client.blade.php
+++ resources/views/users/add-client.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="w-full md:w-1/2">
                     <label for="name" class="block text-[14px] font-[400] leading-[16px] text-[#000000] mb-[5px]">Last Name <strong class="text-[#f83434]">*</strong></label>
-                    <input type="text" name="last_name" id="last_name" value="{{ ucwords(old('last_name') ? old('last_name') : $newClient->last_name)}}" class="w-full h-[45px] border-[1px] border-[#0000001A] text-[14px] font-[400] leading-[16px] text-[#000000] tracking-[0.01em] px-[15px] py-[10px] rounded-[10px] !outline-none" placeholder="Enter Name" required>
+                    <input type="text" name="lastName" id="last_name" value="{{ ucwords(old('last_name') ? old('last_name') : $newClient->last_name)}}" class="w-full h-[45px] border-[1px] border-[#0000001A] text-[14px] font-[400] leading-[16px] text-[#000000] tracking-[0.01em] px-[15px] py-[10px] rounded-[10px] !outline-none" placeholder="Enter Name" required>
                     @error('last_name')
                     <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
